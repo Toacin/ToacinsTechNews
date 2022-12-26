46c2b32f53e05a8af5d370eb4443fda80b14a71e
Toacin <toacinp@outlook.com>
2022-12-26 16:57:27 -0700

    basic starter code

---
+++ src/main/java/com/ToacinsTechNews/ToacinsTechNewsApplication.java
@@ -0,0 +1,13 @@
+package com.ToacinsTechNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToacinsTechNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToacinsTechNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ToacinsTechNews/ToacinsTechNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ToacinsTechNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToacinsTechNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
